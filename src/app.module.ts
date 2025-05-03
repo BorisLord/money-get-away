25fe4eecd32faab9035b82556b3ab3344976cd57
bopad <borisdelord@proton.me>
2025-05-03 22:57:17 +0200

    import item, resto, user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RestaurantsModule } from './restaurants/restaurant.module';
+import { UsersModule } from './users/user.module';
+import { ItemsModule } from './items/item.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    RestaurantsModule,
+    UsersModule,
+    ItemsModule,
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
