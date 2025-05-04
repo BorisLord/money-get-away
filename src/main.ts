130f72de54fc8ea594714e5872303997fa784ee6
bopad <borisdelord@proton.me>
2025-05-04 23:25:55 +0200

    use validation global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Ne garde que les propriétés définies dans le DTO
+      forbidNonWhitelisted: true, // Rejette les requêtes avec des propriétés non attendues
+      transform: true, // Transforme le payload en instance du DTO
+      transformOptions: {
+        enableImplicitConversion: true, // Aide pour la conversion implicite des types primitifs
+      },
+    }),
+  );
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  Logger.log(`App run on :${port}`);
 }
 
 void bootstrap();
