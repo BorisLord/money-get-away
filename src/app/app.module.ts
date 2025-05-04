1d901c680b0eca9e7d76fcffb8c2aa621366cf86
bopad <borisdelord@proton.me>
2025-05-04 23:25:34 +0200

    new folder for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RestaurantsModule } from './restaurants/restaurants.module';
-import { UsersModule } from './users/users.module';
-import { ItemsModule } from './items/items.module';
+import { RestaurantsModule } from '../restaurants/restaurants.module';
+import { UsersModule } from '../users/users.module';
+import { ItemsModule } from '../items/items.module';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-import { RestaurantsService } from './restaurants/restaurants.service';
+import { DatabaseModule } from '../database/database.module';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { RestaurantsService } from './restaurants/restaurants.service';
     UsersModule,
     ItemsModule,
     DatabaseModule,
+    AuthModule,
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
   ],
   controllers: [AppController],
